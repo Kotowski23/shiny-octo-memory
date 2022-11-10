32281c99ece02e29bfd54d2a6759968e0ca05f72
Kotowski23 <103529763+Kotowski23@users.noreply.github.com>
2022-11-10 21:22:00 +0100

    Add files via upload

---
+++ Projekt1/Projekt1/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Projekt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(634, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "buy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(659, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "buy";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(695, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(41, 21);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "buy";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(585, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(635, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "double click";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(292, 214);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(155, 64);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "start";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Projekt1.Properties.Resources.bucket;
+            this.pictureBox2.Location = new System.Drawing.Point(605, 135);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 51);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::Projekt1.Properties.Resources.bucket_zombie;
+            this.button9.ForeColor = System.Drawing.Color.Cyan;
+            this.button9.Location = new System.Drawing.Point(760, 374);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 172);
+            this.button9.TabIndex = 13;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Projekt1.Properties.Resources.sunflower1;
+            this.pictureBox1.Location = new System.Drawing.Point(585, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 50);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Projekt1.Properties.Resources.zombie1;
+            this.button1.ForeColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(321, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 147);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(655, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "new skin (and more)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(605, 183);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(645, 275);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Projekt1.Properties.Resources.wallnut;
+            this.pictureBox3.Location = new System.Drawing.Point(645, 224);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(44, 54);
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Projekt1.Properties.Resources.sun2;
+            this.pictureBox5.Location = new System.Drawing.Point(48, 2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(36, 35);
+            this.pictureBox5.TabIndex = 21;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Projekt1.Properties.Resources.sunflower1;
+            this.pictureBox6.Location = new System.Drawing.Point(-23, 374);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(43, 50);
+            this.pictureBox6.TabIndex = 22;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Projekt1.Properties.Resources.wallnut;
+            this.pictureBox7.Location = new System.Drawing.Point(760, 374);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(44, 54);
+            this.pictureBox7.TabIndex = 23;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::Projekt1.Properties.Resources.wallnutwall1;
+            this.pictureBox8.Location = new System.Drawing.Point(754, 374);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(50, 263);
+            this.pictureBox8.TabIndex = 24;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::Projekt1.Properties.Resources.wallnutwall1;
+            this.pictureBox9.Location = new System.Drawing.Point(760, 374);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(50, 263);
+            this.pictureBox9.TabIndex = 25;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(692, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "nutWALL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projekt1.Properties.Resources.background6;
+            this.ClientSize = new System.Drawing.Size(769, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Projekt1/Projekt1/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection;
+
+namespace Projekt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0; // Define i at class level
+
+        public object Class1 { get; private set; }
+        
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            Random x = new Random();
+
+            
+            if (textBox2.Text == "active")
+            {
+                i = i + 2;
+            }
+            else
+            {
+                 i++;
+            }
+
+            string wallnut = textBox4.Text;
+            if (wallnut == "active")
+            {
+                Point pt2 = new Point(
+                    int.Parse(x.Next(250, 350).ToString()),
+                    int.Parse(x.Next(50, 150).ToString())
+                   );
+                button1.Location = pt2;
+            }
+            else
+            {
+                Point pt = new Point(
+                     int.Parse(x.Next(120, 450).ToString()),
+                     int.Parse(x.Next(50, 150).ToString())
+                    );
+
+                button1.Location = pt;
+            }
+            textBox1.Text = i.ToString();
+            
+
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            string a = "not active";
+            string afive = textBox2.Text;
+             if (i >= 50 && afive == "50$")
+            {
+                i = i - 50;
+                textBox1.Text = i.ToString();
+
+                a = "active";
+                textBox2.Text = a;
+                pictureBox6.Visible = true;
+
+                int x1 = 160;
+                int y1 = 317;
+                Point pt = new Point(
+                     int.Parse(x1.ToString()),
+                     int.Parse(y1.ToString())
+                    );
+
+                pictureBox6.Location = pt;
+            }
+
+           
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+           
+            int afive = 0;
+            textBox2.Text = "50$";
+            textBox3.Text = "60$";
+            textBox4.Text = "80$";
+            button8.Visible = false;
+            button1.Visible = true;
+            button9.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox8.Visible = false;
+            pictureBox9.Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string b;
+            string afive = textBox4.Text;
+
+
+            if (i >= 80 && afive == "80$")
+            {
+               
+                i = i - 80;
+                textBox1.Text = i.ToString();
+
+                b = "active";
+                textBox4.Text = b;
+                pictureBox7.Visible = true;
+                pictureBox8.Visible = true;
+                pictureBox9.Visible = true;
+                int x1 = 432;
+                int y1 = 32;
+                Point pt = new Point(
+                     int.Parse(x1.ToString()),
+                     int.Parse(y1.ToString())
+                    );
+
+                pictureBox9.Location = pt;
+
+                int x2 = 204;
+                int y2 = 32;
+                Point pt2 = new Point(
+                     int.Parse(x2.ToString()),
+                     int.Parse(y2.ToString())
+                    );
+
+                pictureBox8.Location = pt2;
+
+                int x3 = 260;
+                int y3 = 67;
+                Point pt3 = new Point(
+                     int.Parse(x3.ToString()),
+                     int.Parse(y3.ToString())
+                    );
+
+                button9.Location = pt3;
+
+                int x4 = 238;
+                int y4 = 313;
+                Point pt4 = new Point(
+                     int.Parse(x4.ToString()),
+                     int.Parse(y4.ToString())
+                    );
+
+                pictureBox7.Location = pt4;
+            }
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+
+            Random x = new Random();
+
+
+            if (textBox3.Text == "active")
+            {
+                i = i + 5;
+            }
+            else
+            {
+                if (textBox2.Text == "active")
+                {
+                    i = i + 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            string wallnut = textBox4.Text;
+            if (wallnut == "active")
+            {
+                Point pt2 = new Point(
+                    int.Parse(x.Next(272, 300).ToString()),
+                    int.Parse(x.Next(50, 120).ToString())
+                   );
+                button9.Location = pt2;
+            }
+            else
+            {
+                Point pt = new Point(
+                     int.Parse(x.Next(120, 450).ToString()),
+                     int.Parse(x.Next(50, 150).ToString())
+                    );
+
+                button9.Location = pt;
+            }
+               
+            textBox1.Text = i.ToString();
+          
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            string b;
+            string afive = textBox3.Text;
+
+          
+            if (i >= 60 && afive == "60$")
+            {
+                button1.Visible = false;
+                button9.Visible = true;
+                i = i - 60;
+                textBox1.Text = i.ToString();
+
+                b = "active";
+                textBox3.Text = b;
+                int x1 = 260;
+                int y1 = 67;
+                Point pt = new Point(
+                     int.Parse(x1.ToString()),
+                     int.Parse(y1.ToString())
+                    );
+
+               button9.Location = pt;
+            }
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+          
+        }
+
+       
+    }
+}
